9f05d1a5007bc1ee29c3b559a87fdc27127ae4c3
jonasreinhard <reinhardj@cip.ifi.lmu.de>
2024-12-06 22:42:30 +0100

    Add function to create new grid

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
 const container = document.querySelector("#container")
 
-const AMOUNT_OF_SQUARES_ROW = 16
-const AMOUNT_OF_SQUARES_COLUMN = 16
-for(i=0;i<AMOUNT_OF_SQUARES_COLUMN;i++){
+let padSize = 16
+let squareSize = 500/padSize
+createSketchPad(padSize)
+function createSketchPad(padSize){
+    for(i=0;i<padSize;i++){
         const row  = document.createElement("div")
         row.style.display = "flex"
         row.classList.add("row");
         container.append(row)
-    for(j=0;j<AMOUNT_OF_SQUARES_ROW;j++){
-        const span  = document.createElement("span")
-        span.classList.add("square");
-        span.style.borderStyle= "solid"
-        span.style.borderColor = "black"
-        span.style.backgroundColor ="red"
-        span.style.height = "50px"
-        span.style.width = "50px"
-        row.appendChild(span)
+        for(j=0;j<padSize;j++){
+            const span  = document.createElement("span")
+            span.classList.add("square");
+            span.style.borderStyle= "solid"
+            span.style.borderColor = "black"
+            span.style.backgroundColor ="red"
+            span.style.height= `${squareSize}px`
+            span.style.width = `${squareSize}px`
+            row.appendChild(span)
+        }
+    }
+    const squares = document.querySelectorAll(".square")
+    squares.forEach((square) => {
+        square.addEventListener("mouseover", () => square.style.backgroundColor ="black")
+    })
+}
+function getGridSizeUserInput(){
+    let userInput = Number.parseInt(prompt("Enter new grid size"))
+    squareSize = 500/userInput
+    if(userInput<=100){
+        console.log(container.querySelectorAll(".row").forEach(item => item.remove()))
+        createSketchPad(userInput)
     }
 }
 
-const newPadButton = document.querySelector("#new-pad-button")
-const squares = document.querySelectorAll(".square")
-squares.forEach((square) => {
-    square.addEventListener("mouseover", () => square.style.backgroundColor ="black")
-})
 
-newPadButton.addEventListener("click",() => prompt())
+
+const newPadButton = document.querySelector("#new-pad-button")
+
+
+
+
+newPadButton.addEventListener("click",() => getGridSizeUserInput())
+
